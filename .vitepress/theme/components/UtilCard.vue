0bfd92b27d19fd9339c3c508caa2e298072cf8c5
zouhai <zou.hai@outlook.com>
2023-11-22 23:53:39 +0800

    feat: 添加卡片组件

---
+++ .vitepress/theme/components/UtilCard.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="util-card-container" :title="props.description">
+		<a
+			class="title"
+			:href="props.link"
+			target="_blank"
+		>
+			{{ props.title }}
+		</a>
+		<div class="desc">
+			{{ props.description }}
+		</div>
+	</div>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: String,
+    link: String,
+    description: String,
+});
+</script>
+
+<style lang="scss">
+.util-card-container {
+	display: inline-flex;
+	flex-direction: column;
+	row-gap: 1rem;
+	padding: 1.2rem 0.6rem;
+	text-align: center;
+	border: 1px solid var(--vp-c-gray-1);
+	border-radius: 8px;
+	width: 10rem;
+	box-sizing: border-box;
+
+	.title {
+		font-size: 1.1rem;
+		cursor: pointer;
+	}
+
+	.desc {
+		white-space: nowrap;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		font-size: 0.9rem;
+	}
+}
+</style>
--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,6 +1,7 @@
 // https://vitepress.dev/guide/custom-theme
 import { PoseidonTheme } from 'vitepress-theme-poseidon';
 import FriendLinks from './components/FriendLinks.vue';
+import UtilCard from './components/UtilCard.vue';
 import './style.css';
 
 /** @type {import('vitepress').Theme} */
@@ -8,5 +9,6 @@ export default {
 	extends: PoseidonTheme,
 	enhanceApp({ app }) {
 		app.component("FriendLinks", FriendLinks);
+		app.component("UtilCard", UtilCard);
 	}
 }
